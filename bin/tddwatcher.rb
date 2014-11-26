248faac48fad3c1716fc4e1f92f3dc2ba8cdda66
botp <botp.eeweb@gmail.com>
2014-11-26 17:01:39 +0800

    use osx notification instead of say

--- bin/tddwatcher.rb
+++ bin/tddwatcher.rb
@@ -4,18 +4,22 @@ require "#{runner_path}/bin/mktest_template.rb"
 require "#{runner_path}/bin/mktest_runner.rb"
 
 def say_n_print text
-	p text
-	system "say '#{text}' "  # &"
+	puts text
+	cmd = %Q|osascript -e 'display notification "#{text}" with title "Test notification"'|
+	system cmd
+	# system "say '#{text}' "  # &"
 end
 
 def cmd_runner pathname
 	# say_n_print "running test"    #  "#{File.basename(pathname)}..."
+	say_n_print "running test #{pathname}..."
 	puts "running test #{pathname}..."
 	mktest_runner pathname
 end
 
 def cmd_creator pathname
 	# say_n_print "creating template"  # for #{pathname}..."
+	say_n_print "creating template for #{pathname}..."
 	puts "creating template #{pathname}..."
 	mktest_template pathname
 end
